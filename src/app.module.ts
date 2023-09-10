e452714014b68aff34c6a680733ad64a1a9ccfe7
dongjin2008 <80329234+dongjin2008@users.noreply.github.com>
2023-09-10 18:08:32 +0200

    Error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { Todo } from './todo/entities/todo.entity';
+import { config } from "dotenv";
+import * as process from "process";
+config();
 
 @Module({
   imports: [
       TypeOrmModule.forRoot({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        password: 'djkim0318',
-        username: 'postgres',
-        database: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        password: process.env.DB_PASSWORD,
+        username: process.env.DB_USERNAME,
+        database: process.env.DB_NAME,
         entities: [Todo],
         synchronize: true,
         logging: true,
